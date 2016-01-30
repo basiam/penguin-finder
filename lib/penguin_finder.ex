bd159e4248919845a572b3d99c83eb1683e5bfe6
Basia Madej <basia@lunarlogic.io>
2016-01-30 18:52:25 +0100

    sudo make penguins

--- lib/penguin_finder.ex
+++ lib/penguin_finder.ex
@@ -1,4 +1,8 @@
 defmodule PenguinFinder do
+  require Record
+
+  Record.defrecord :penguin, [name: nil, rank: nil]
+
   @doc """
   Find index file in given directory
 
@@ -9,6 +13,28 @@ defmodule PenguinFinder do
   def find_index(directory) do
     files = Path.wildcard(Path.join(directory, "*.txt"))
     Enum.find files, &(is_index?(&1))
+  end
+
+  def extract_data(index_file) do
+    File.open! index_file, fn(pid) ->
+      IO.read(pid, :line)
+      extract_info(pid, [])
+    end
+  end
+
+  defp extract_info(pid, acc) do
+    case IO.read(pid, :line) do
+      :eof -> Enum.reverse(acc)
+      info when is_binary(info) ->
+        path = IO.read(pid, :line)
+        extract_info(pid, info, path, acc)
+    end
+  end
+
+  defp extract_info(pid, info, path, acc) do
+    [name, rank] = String.split(info, "; ")
+    record = penguin(name: String.strip(name), rank: String.strip(rank))
+    extract_info(pid, [record|acc])
   end
 
   defp is_index?(file) do
--- test/penguin_finder_test.exs
+++ test/penguin_finder_test.exs
@@ -2,11 +2,19 @@ defmodule PenguinFinderTest do
   use ExUnit.Case, async: true
   doctest PenguinFinder
 
+  @index_file "secrets/CRNCLHYEMR.txt"
+
   test "find the index file in a directory" do
-    assert PenguinFinder.find_index("secrets") == "secrets/CRNCLHYEMR.txt"
+    assert PenguinFinder.find_index("secrets") == @index_file
   end
 
   test "return null if no index file available" do
     assert PenguinFinder.find_index("worngs") == nil
+  end
+
+  test "extracts data from index file" do
+    data = PenguinFinder.extract_data(@index_file)
+    assert List.first(data) ==
+      PenguinFinder.penguin(name: "Skipper", rank: "Leader")
   end
 end
